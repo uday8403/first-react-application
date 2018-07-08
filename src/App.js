2386ca459fbe9ae3f529b798003522be9c20487e
Udayveer Singh <uady.uday1430@gmail.com>
2018-07-08 17:46:54 +0530

    minima changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import Person from "./Person/Person";
+import Person from "./Person/Person"; 
 class App extends Component {
   render() {
     // return React.createElement('div',{className:'App'},React.createElement('h1',null,'How you doin!'));
@@ -9,6 +9,7 @@ class App extends Component {
      <div className="App">
         <div>
           <h1>Hi....this is the first React Application</h1>
+          <p>Let's see who all are here...</p>
         </div>
         <Person name="UD" age={25}></Person>
         <Person name="Mitthu" age={24}></Person>
