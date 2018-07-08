eb67cdbc2ba7ceb972557cd116543432df33e8ff
Udayveer Singh <uady.uday1430@gmail.com>
2018-07-09 00:21:52 +0530

    commit2

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,14 @@
 import React, {Component} from "react";
+ 
+import './Person.css';
 
 const person=(props)=>{
    return (
-    <p>I am {props.name}. My age is {props.age}. My random score is:{Math.floor(Math.random() * 100)}</p>
-
-   ); 
+    <div className="Person">  
+        <p>I am {props.name}. My age is {props.age}. My random score is:{Math.floor(Math.random() * 100)}</p>
+        <input onChange={props.changed} value={props.name} type="text"/>
+    </div> 
+   );  
 }
 
-export default  person;
+export default  person; 
